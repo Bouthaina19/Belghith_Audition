e12863794edaa1aff7374ccd1e345feb508002ed
Bouthaina <boutheinabelg1@gmail.com>
2025-04-12 19:00:43 +0100

    configuration

--- audition/settings.py
+++ audition/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z3^8=2q9=b-sjadbp&3-*4l*cj-(%8ow^*h^wsw$6gj82t_odu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'bouthaina123.pythonanywhere.com']
 
 
 # Application definition
